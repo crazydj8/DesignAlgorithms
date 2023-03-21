10a80028835c16ac97e91363a5e3e7ccccef6d62
crazydj8 <akshataryan8@gmail.com>
2023-03-21 16:15:26 +0530

    Array uniqueness in Python

---
+++ Misc. Algos/Non_recursive_algos/uniquearray.py
@@ -0,0 +1,24 @@
+# Finding whether the array has all distinct elements or not
+#code by Akshat Aryan
+
+#mentioned algorithm:
+def unique(arr):
+    for i in range(0, len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if(arr[i] == arr[j]):
+                return False
+    return True
+
+#another algorithm possible in python
+def unique_set(arr):
+    #set only contains distinct elements
+    #if number of elements in the set and array are same, means array has unique elements
+    if(len(arr) == len(set(arr))):
+        return True
+    else:
+        return False
+
+if(__name__ == "__main__"):
+    arr = list(map(lambda x: int(x), input("Enter the elements separated by space:").split()))
+    print("Using Levitin's Algo, uniqueness =", unique(arr))
+    print("Using the other Algo, uniqueness =", unique_set(arr))
